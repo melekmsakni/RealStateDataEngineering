a3d48c08bf28425e02e8e353c00e78f99bdc43d4
melekmsakni <msaknimelek2@gmail.com>
2024-08-19 02:18:13 +0100

    fixing the java in airflow container

--- dags/data_processing_and_storage_dag.py
+++ dags/data_processing_and_storage_dag.py
@@ -28,19 +28,10 @@ spark_submit_task = SparkSubmitOperator(
     task_id='spark_submit_task',
     conn_id='spark_container',  # The connection ID you created for Spark
     application='/opt/bitnami/spark/jobs/spark-consumer.py',  # Path to your Spark job script
-    executor_memory='2g',
-    driver_memory='2g',
-    total_executor_cores=2,
     packages='com.datastax.spark:spark-cassandra-connector_2.13:3.4.1,org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1',  # Additional packages
     dag=dag,
 )
-from airflow.operators.bash import BashOperator
 
-check_env_task = BashOperator(
-    task_id='check_env',
-    bash_command='echo $JAVA_HOME && ps --version',
-    dag=dag,
-)
 
-check_env_task >> spark_submit_task
+spark_submit_task
 
